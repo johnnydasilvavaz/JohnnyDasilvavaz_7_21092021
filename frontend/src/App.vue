8c2ffdc67f05ab349210daaacac00165c9566d8d
Johnny DA SILVA VAZ <31535224+johnnydasilvavaz@users.noreply.github.com>
2021-10-01 00:40:35 +0200

    Init front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,11 @@
 <template>
-  <img class="login__logo" alt="Vue logo" src="./assets/icon-above-font.png">
-  <Login msg="Bienvenue"/>
-</template>
-
-<script>
-import Login from './components/Login.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Login
-  }
-}
-</script>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>./views/Login.vue
 
 <style>
 #app {
@@ -21,11 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-.login__logo {
-  max-width: 15rem;
+#nav {
+  padding: 30px;
 }
 
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
